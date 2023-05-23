095dfc09daf0109c0ba3a89378e8b411407ab93d
xavimaestas <102766454+xavimaestas@users.noreply.github.com>
2023-05-23 16:50:55 -0600

    About, Home, Nav and Footer
    All have been intergrated with React Router and Linked. All CSS finished on each component.

---
+++ vanliferouter/src/Routes/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home(){
+    return (
+        <div className="home-main">
+            <div className="home-content">
+                <h1>You got the travel plans, we got the travel vans.</h1>
+                <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+                <a>Find your van</a>
+            </div>
+        </div>
+    )
+  }
